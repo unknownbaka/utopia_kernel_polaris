MotorolaMobilityLLC/vendor-qcom-opensource-wlan-qcacld-3.0

qcacld-3.0: Trigger pktlog as part of hb failure/kickout event
When Driver does disconnect as part of heartbeat failure
or kickout event, trigger fatal event to capture pktlog.

Change-Id: I43a486afeccc4acd4be542357b803d492afde9c0
CRs-Fixed: 2030718

core/sme/src/csr/csr_api_roam.c
@@ -19242,6 +19242,11 @@ void csr_process_ho_fail_ind(tpAniSirGlobal pMac, void *pMsgBuf)
 		  "LFR3:Issue Disconnect on session %d", sessionId);
 	csr_roam_disconnect(pMac, sessionId,
 			eCSR_DISCONNECT_REASON_ROAM_HO_FAIL);
+	if (pMac->roam.configParam.enable_fatal_event)
+		cds_flush_logs(WLAN_LOG_TYPE_NON_FATAL,
+				WLAN_LOG_INDICATOR_HOST_DRIVER,
+				WLAN_LOG_REASON_ROAM_HO_FAILURE,
+				true, false);
 }
 #endif /* WLAN_FEATURE_ROAM_OFFLOAD */
 

unknownbaka/utopia_kernel_polaris

core/sme/src/csr/csr_api_roam.c
@@ -19131,6 +19131,11 @@ void csr_process_ho_fail_ind(tpAniSirGlobal mac_ctx, void *pMsgBuf)
 		  "LFR3:Issue Disconnect on session %d", sessionId);
 	csr_roam_disconnect(mac_ctx, sessionId,
 			eCSR_DISCONNECT_REASON_ROAM_HO_FAIL);
+	if (mac_ctx->roam.configParam.enable_fatal_event)
+		cds_flush_logs(WLAN_LOG_TYPE_NON_FATAL,
+				WLAN_LOG_INDICATOR_HOST_DRIVER,
+				WLAN_LOG_REASON_ROAM_HO_FAILURE,
+				true, false);
 }
 #endif /* WLAN_FEATURE_ROAM_OFFLOAD */
 