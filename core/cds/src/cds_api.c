OnePlusOSS/android_kernel_oneplus_msm8998

qcacld-3.0: Add support to get IPA SMMU status
Add support to get IPA SMMU status

Change-Id: I0e476b465bcaa2c5e8ae58c0783265e78ebc8181
CRs-Fixed: 2176317

core/cds/src/cds_api.c
@@ -2876,21 +2876,44 @@ cds_print_htc_credit_history(uint32_t count, qdf_abstract_print *print,
 #endif
 
 #ifdef ENABLE_SMMU_S1_TRANSLATION
-void cds_smmu_mem_map_setup(qdf_device_t osdev)
+QDF_STATUS cds_smmu_mem_map_setup(qdf_device_t osdev)
 {
 	int attr = 0;
+	bool ipa_smmu_enable;
 	struct dma_iommu_mapping *mapping = pld_smmu_get_mapping(osdev->dev);
 
 	osdev->smmu_s1_enabled = false;
-	if (!mapping) {
-		cds_info("No SMMU mapping present");
-		return;
+
+	ipa_smmu_enable = qdf_get_ipa_smmu_status();
+	if (ipa_smmu_enable)
+		cds_info("SMMU enabled from IPA side");
+	else
+		cds_info("SMMU not enabled from IPA side");
+
+	if (mapping && ((iommu_domain_get_attr(mapping->domain,
+			 DOMAIN_ATTR_S1_BYPASS, &attr) == 0) &&
+			 !attr)) {
+		cds_info("SMMU enabled from WLAN side");
+		if (ipa_smmu_enable) {
+			cds_info("SMMU enabled from both IPA and WLAN side");
+			osdev->smmu_s1_enabled = true;
+			return QDF_STATUS_SUCCESS;
+		} else {
+			cds_err("SMMU mismatch: IPA: disable, WLAN: enable");
+			return QDF_STATUS_E_FAILURE;
+		}
+	} else {
+		cds_info("No SMMU mapping present or SMMU disabled from WLAN side");
+		if (ipa_smmu_enable) {
+			cds_err("SMMU mismatch: IPA: enable, WLAN: disable");
+			return QDF_STATUS_E_FAILURE;
+		} else {
+			cds_info("SMMU diabled from both IPA and WLAN side");
+			return QDF_STATUS_SUCCESS;
+		}
 	}
 
-	if ((iommu_domain_get_attr(mapping->domain,
-				   DOMAIN_ATTR_S1_BYPASS, &attr) == 0) &&
-				   !attr)
-		osdev->smmu_s1_enabled = true;
+	return QDF_STATUS_SUCCESS;
 }
 
 #ifdef IPA_OFFLOAD
@@ -2906,9 +2929,10 @@ int cds_smmu_map_unmap(bool map, uint32_t num_buf, qdf_mem_info_t *buf_arr)
 #endif
 
 #else
-void cds_smmu_mem_map_setup(qdf_device_t osdev)
+QDF_STATUS cds_smmu_mem_map_setup(qdf_device_t osdev)
 {
 	osdev->smmu_s1_enabled = false;
+	return QDF_STATUS_SUCCESS;
 }
 
 int cds_smmu_map_unmap(bool map, uint32_t num_buf, qdf_mem_info_t *buf_arr)

unknownbaka/utopia_kernel_polaris

core/cds/src/cds_api.c
@@ -2845,21 +2845,44 @@ void cds_incr_arp_stats_tx_tgt_acked(void)
 }
 
 #ifdef ENABLE_SMMU_S1_TRANSLATION
-void cds_smmu_mem_map_setup(qdf_device_t osdev)
+QDF_STATUS cds_smmu_mem_map_setup(qdf_device_t osdev)
 {
 	int attr = 0;
+	bool ipa_smmu_enable;
 	struct dma_iommu_mapping *mapping = pld_smmu_get_mapping(osdev->dev);
 
 	osdev->smmu_s1_enabled = false;
-	if (!mapping) {
-		cds_info("No SMMU mapping present");
-		return;
+
+	ipa_smmu_enable = qdf_get_ipa_smmu_enabled();
+	if (ipa_smmu_enable)
+		cds_info("SMMU enabled from IPA side");
+	else
+		cds_info("SMMU not enabled from IPA side");
+
+	if (mapping && ((iommu_domain_get_attr(mapping->domain,
+			 DOMAIN_ATTR_S1_BYPASS, &attr) == 0) &&
+			 !attr)) {
+		cds_info("SMMU enabled from WLAN side");
+		if (ipa_smmu_enable) {
+			cds_info("SMMU enabled from both IPA and WLAN side");
+			osdev->smmu_s1_enabled = true;
+			return QDF_STATUS_SUCCESS;
+		} else {
+			cds_err("SMMU mismatch: IPA: disable, WLAN: enable");
+			return QDF_STATUS_E_FAILURE;
+		}
+	} else {
+		cds_info("No SMMU mapping present or SMMU disabled from WLAN side");
+		if (ipa_smmu_enable) {
+			cds_err("SMMU mismatch: IPA: enable, WLAN: disable");
+			return QDF_STATUS_E_FAILURE;
+		} else {
+			cds_info("SMMU diabled from both IPA and WLAN side");
+			return QDF_STATUS_SUCCESS;
+		}
 	}
 
-	if ((iommu_domain_get_attr(mapping->domain,
-				   DOMAIN_ATTR_S1_BYPASS, &attr) == 0) &&
-				   !attr)
-		osdev->smmu_s1_enabled = true;
+	return QDF_STATUS_SUCCESS;
 }
 
 #ifdef IPA_OFFLOAD
@@ -2875,9 +2898,10 @@ int cds_smmu_map_unmap(bool map, uint32_t num_buf, qdf_mem_info_t *buf_arr)
 #endif
 
 #else
-void cds_smmu_mem_map_setup(qdf_device_t osdev)
+QDF_STATUS cds_smmu_mem_map_setup(qdf_device_t osdev)
 {
 	osdev->smmu_s1_enabled = false;
+	return QDF_STATUS_SUCCESS;
 }
 
 int cds_smmu_map_unmap(bool map, uint32_t num_buf, qdf_mem_info_t *buf_arr)