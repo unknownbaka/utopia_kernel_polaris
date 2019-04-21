bittencourtjulival/eclipse_kernel_xiaomi_stone

qcacld-3.0: Fix behavior of SET_FCC_CHANNEL
Expectation of user is fcc constraint should be applied when user input
is zero and should be cleared when input is negative value. But current
driver behavior is out of sync.

To address this, invert the user-input before invoking regulatory
component api ucfg_reg_set_fcc_constraint(). Also ensure reduced
tx power is effective in calculating maximum tx power while connecting
to access-point.

Change-Id: I73485685b70b805107a52080c64efde30e807dc3
CRs-Fixed: 2429993

core/mac/src/pe/lim/lim_ft.c
@@ -529,6 +529,7 @@ void lim_fill_ft_session(struct mac_context *mac,
 	int8_t regMax;
 	tSchBeaconStruct *pBeaconStruct;
 	ePhyChanBondState cbEnabledMode;
+	struct lim_max_tx_pwr_attr tx_pwr_attr = {0};
 
 	pBeaconStruct = qdf_mem_malloc(sizeof(tSchBeaconStruct));
 	if (!pBeaconStruct)
@@ -681,10 +682,15 @@ void lim_fill_ft_session(struct mac_context *mac,
 	ft_session->isFastRoamIniFeatureEnabled =
 		pe_session->isFastRoamIniFeatureEnabled;
 
+	tx_pwr_attr.reg_max = regMax;
+	tx_pwr_attr.ap_tx_power = localPowerConstraint;
+	tx_pwr_attr.ini_tx_power = mac->mlme_cfg->power.max_tx_power;
+	tx_pwr_attr.frequency =
+		wlan_reg_get_channel_freq(mac->pdev,
+					  ft_session->currentOperChannel);
+
 #ifdef FEATURE_WLAN_ESE
-	ft_session->maxTxPower =
-		lim_get_max_tx_power(regMax, localPowerConstraint,
-				     mac->mlme_cfg->power.max_tx_power);
+	ft_session->maxTxPower = lim_get_max_tx_power(mac, &tx_pwr_attr);
 #else
 	ft_session->maxTxPower = QDF_MIN(regMax, (localPowerConstraint));
 #endif

core/mac/src/pe/rrm/rrm_api.c
@@ -228,6 +228,7 @@ rrm_process_link_measurement_request(struct mac_context *mac,
 	tSirMacLinkReport LinkReport;
 	tpSirMacMgmtHdr pHdr;
 	int8_t currentRSSI = 0;
+	struct lim_max_tx_pwr_attr tx_pwr_attr = {0};
 
 	pe_debug("Received Link measurement request");
 
@@ -237,10 +238,11 @@ rrm_process_link_measurement_request(struct mac_context *mac,
 	}
 	pHdr = WMA_GET_RX_MAC_HEADER(pRxPacketInfo);
 
-	LinkReport.txPower = lim_get_max_tx_power(
-					pe_session->def_max_tx_pwr,
-					pLinkReq->MaxTxPower.maxTxPower,
-					mac->mlme_cfg->power.max_tx_power);
+	tx_pwr_attr.reg_max = pe_session->def_max_tx_pwr;
+	tx_pwr_attr.ap_tx_power = pLinkReq->MaxTxPower.maxTxPower;
+	tx_pwr_attr.ini_tx_power = mac->mlme_cfg->power.max_tx_power;
+
+	LinkReport.txPower = lim_get_max_tx_power(mac, &tx_pwr_attr);
 
 	if ((LinkReport.txPower != (uint8_t) (pe_session->maxTxPower)) &&
 	    (QDF_STATUS_SUCCESS == rrm_send_set_max_tx_power_req(mac,

unknownbaka/utopia_kernel_polaris

core/mac/src/pe/lim/lim_ft.c
@@ -530,6 +530,7 @@ void lim_fill_ft_session(tpAniSirGlobal pMac,
 	int8_t regMax;
 	tSchBeaconStruct *pBeaconStruct;
 	ePhyChanBondState cbEnabledMode;
+	struct lim_max_tx_pwr_attr tx_pwr_attr = {0};
 
 	pBeaconStruct = qdf_mem_malloc(sizeof(tSchBeaconStruct));
 	if (NULL == pBeaconStruct) {
@@ -687,10 +688,16 @@ void lim_fill_ft_session(tpAniSirGlobal pMac,
 	pftSessionEntry->isFastRoamIniFeatureEnabled =
 		psessionEntry->isFastRoamIniFeatureEnabled;
 
+	tx_pwr_attr.reg_max = regMax;
+	tx_pwr_attr.ap_tx_power = localPowerConstraint;
+	tx_pwr_attr.ini_tx_power = pMac->roam.configParam.nTxPowerCap;
+	tx_pwr_attr.frequency =
+		wlan_reg_get_channel_freq(pMac->pdev,
+					  pftSessionEntry->currentOperChannel);
+
 #ifdef FEATURE_WLAN_ESE
 	pftSessionEntry->maxTxPower =
-		lim_get_max_tx_power(regMax, localPowerConstraint,
-				     pMac->roam.configParam.nTxPowerCap);
+		lim_get_max_tx_power(pMac, &tx_pwr_attr);
 #else
 	pftSessionEntry->maxTxPower = QDF_MIN(regMax, (localPowerConstraint));
 #endif

core/mac/src/pe/rrm/rrm_api.c
@@ -250,6 +250,7 @@ rrm_process_link_measurement_request(tpAniSirGlobal pMac,
 	tSirMacLinkReport LinkReport;
 	tpSirMacMgmtHdr pHdr;
 	int8_t currentRSSI = 0;
+	struct lim_max_tx_pwr_attr tx_pwr_attr = {0};
 
 	pe_debug("Received Link measurement request");
 
@@ -259,10 +260,11 @@ rrm_process_link_measurement_request(tpAniSirGlobal pMac,
 	}
 	pHdr = WMA_GET_RX_MAC_HEADER(pRxPacketInfo);
 
-	LinkReport.txPower = lim_get_max_tx_power(pSessionEntry->def_max_tx_pwr,
-						pLinkReq->MaxTxPower.maxTxPower,
-						  pMac->roam.configParam.
-						  nTxPowerCap);
+	tx_pwr_attr.reg_max = pSessionEntry->def_max_tx_pwr;
+	tx_pwr_attr.ap_tx_power = pLinkReq->MaxTxPower.maxTxPower;
+	tx_pwr_attr.ini_tx_power = pMac->roam.configParam.nTxPowerCap;
+
+	LinkReport.txPower = lim_get_max_tx_power(pMac, &tx_pwr_attr);
 
 	if ((LinkReport.txPower != (uint8_t) (pSessionEntry->maxTxPower)) &&
 	    (QDF_STATUS_SUCCESS == rrm_send_set_max_tx_power_req(pMac,