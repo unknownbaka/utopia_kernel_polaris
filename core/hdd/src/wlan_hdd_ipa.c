bittencourtjulival/eclipse_kernel_xiaomi_stone

qcacld-3.0: abstract Linux identifiers from IPA component
Abstract Linux based OS identifiers from IPA component in
HDD/OSIF layer.

Change-Id: I82a5db8c796d47d552757b1a450df10c72061177

core/hdd/src/wlan_hdd_ipa.c
@@ -399,7 +399,7 @@ static int hdd_ipa_aggregated_rx_ind(qdf_nbuf_t skb)
 }
 #endif
 
-void hdd_ipa_send_skb_to_network(qdf_nbuf_t skb, qdf_netdev_t dev)
+void hdd_ipa_send_nbuf_to_network(qdf_nbuf_t nbuf, qdf_netdev_t dev)
 {
 	struct hdd_adapter *adapter = (struct hdd_adapter *) netdev_priv(dev);
 	int result;
@@ -408,37 +408,37 @@ void hdd_ipa_send_skb_to_network(qdf_nbuf_t skb, qdf_netdev_t dev)
 	uint32_t enabled;
 
 	if (hdd_validate_adapter(adapter)) {
-		kfree_skb(skb);
+		kfree_skb(nbuf);
 		return;
 	}
 
 	if (cds_is_driver_unloading()) {
-		kfree_skb(skb);
+		kfree_skb(nbuf);
 		return;
 	}
 
 	if ((adapter->device_mode == QDF_SAP_MODE) &&
-	    (qdf_nbuf_is_ipv4_dhcp_pkt(skb) == true)) {
+	    (qdf_nbuf_is_ipv4_dhcp_pkt(nbuf) == true)) {
 		/* Send DHCP Indication to FW */
 		struct qdf_mac_addr *src_mac =
-			(struct qdf_mac_addr *)(skb->data +
+			(struct qdf_mac_addr *)(nbuf->data +
 			QDF_NBUF_SRC_MAC_OFFSET);
 		if (QDF_STATUS_SUCCESS ==
 			hdd_softap_get_sta_id(adapter, src_mac, &sta_id))
-			hdd_inspect_dhcp_packet(adapter, sta_id, skb, QDF_RX);
+			hdd_inspect_dhcp_packet(adapter, sta_id, nbuf, QDF_RX);
 	}
 
-	qdf_dp_trace_set_track(skb, QDF_RX);
+	qdf_dp_trace_set_track(nbuf, QDF_RX);
 
-	hdd_event_eapol_log(skb, QDF_RX);
+	hdd_event_eapol_log(nbuf, QDF_RX);
 	qdf_dp_trace_log_pkt(adapter->vdev_id,
-			     skb, QDF_RX, QDF_TRACE_DEFAULT_PDEV_ID);
-	DPTRACE(qdf_dp_trace(skb,
+			     nbuf, QDF_RX, QDF_TRACE_DEFAULT_PDEV_ID);
+	DPTRACE(qdf_dp_trace(nbuf,
 			     QDF_DP_TRACE_RX_HDD_PACKET_PTR_RECORD,
 			     QDF_TRACE_DEFAULT_PDEV_ID,
-			     qdf_nbuf_data_addr(skb),
-			     sizeof(qdf_nbuf_data(skb)), QDF_RX));
-	DPTRACE(qdf_dp_trace_data_pkt(skb, QDF_TRACE_DEFAULT_PDEV_ID,
+			     qdf_nbuf_data_addr(nbuf),
+			     sizeof(qdf_nbuf_data(nbuf)), QDF_RX));
+	DPTRACE(qdf_dp_trace_data_pkt(nbuf, QDF_TRACE_DEFAULT_PDEV_ID,
 				      QDF_DP_TRACE_RX_PACKET_RECORD, 0,
 				      QDF_RX));
 
@@ -450,9 +450,9 @@ void hdd_ipa_send_skb_to_network(qdf_nbuf_t skb, qdf_netdev_t dev)
 	if (!enabled)
 		hdd_ipa_set_wake_up_idle(true);
 
-	skb->dev = adapter->dev;
-	skb->protocol = eth_type_trans(skb, skb->dev);
-	skb->ip_summed = CHECKSUM_NONE;
+	nbuf->dev = adapter->dev;
+	nbuf->protocol = eth_type_trans(nbuf, nbuf->dev);
+	nbuf->ip_summed = CHECKSUM_NONE;
 
 	cpu_index = wlan_hdd_get_cpu();
 
@@ -464,9 +464,9 @@ void hdd_ipa_send_skb_to_network(qdf_nbuf_t skb, qdf_netdev_t dev)
 	 */
 
 	++adapter->stats.rx_packets;
-	adapter->stats.rx_bytes += skb->len;
+	adapter->stats.rx_bytes += nbuf->len;
 
-	result = hdd_ipa_aggregated_rx_ind(skb);
+	result = hdd_ipa_aggregated_rx_ind(nbuf);
 	if (result == NET_RX_SUCCESS)
 		++adapter->hdd_stats.tx_rx_stats.rx_delivered[cpu_index];
 	else

unknownbaka/utopia_kernel_polaris

core/hdd/src/wlan_hdd_ipa.c
@@ -383,7 +383,7 @@ static int hdd_ipa_aggregated_rx_ind(qdf_nbuf_t skb)
 }
 #endif
 
-void hdd_ipa_send_skb_to_network(qdf_nbuf_t skb, qdf_netdev_t dev)
+void hdd_ipa_send_nbuf_to_network(qdf_nbuf_t nbuf, qdf_netdev_t dev)
 {
 	struct hdd_adapter *adapter = (struct hdd_adapter *) netdev_priv(dev);
 	int result;
@@ -392,23 +392,23 @@ void hdd_ipa_send_skb_to_network(qdf_nbuf_t skb, qdf_netdev_t dev)
 	uint32_t enabled;
 
 	if (hdd_validate_adapter(adapter)) {
-		kfree_skb(skb);
+		kfree_skb(nbuf);
 		return;
 	}
 
 	if (cds_is_driver_unloading()) {
-		kfree_skb(skb);
+		kfree_skb(nbuf);
 		return;
 	}
 
 	if (adapter->device_mode == QDF_SAP_MODE) {
 		/* Send DHCP Indication to FW */
 		struct qdf_mac_addr *src_mac =
-			(struct qdf_mac_addr *)(skb->data +
+			(struct qdf_mac_addr *)(nbuf->data +
 			QDF_NBUF_SRC_MAC_OFFSET);
 		if (QDF_STATUS_SUCCESS ==
 			hdd_softap_get_sta_id(adapter, src_mac, &staid))
-			hdd_inspect_dhcp_packet(adapter, staid, skb, QDF_RX);
+			hdd_inspect_dhcp_packet(adapter, staid, nbuf, QDF_RX);
 	}
 
 	/*
@@ -419,9 +419,9 @@ void hdd_ipa_send_skb_to_network(qdf_nbuf_t skb, qdf_netdev_t dev)
 	if (!enabled)
 		hdd_ipa_set_wake_up_idle(true);
 
-	skb->dev = adapter->dev;
-	skb->protocol = eth_type_trans(skb, skb->dev);
-	skb->ip_summed = CHECKSUM_NONE;
+	nbuf->dev = adapter->dev;
+	nbuf->protocol = eth_type_trans(nbuf, nbuf->dev);
+	nbuf->ip_summed = CHECKSUM_NONE;
 
 	cpu_index = wlan_hdd_get_cpu();
 
@@ -433,24 +433,24 @@ void hdd_ipa_send_skb_to_network(qdf_nbuf_t skb, qdf_netdev_t dev)
 	 */
 	if (adapter->device_mode == QDF_STA_MODE) {
 		++adapter->stats.rx_packets;
-		adapter->stats.rx_bytes += skb->len;
+		adapter->stats.rx_bytes += nbuf->len;
 	}
 
-	qdf_dp_trace_set_track(skb, QDF_RX);
+	qdf_dp_trace_set_track(nbuf, QDF_RX);
 
-	hdd_event_eapol_log(skb, QDF_RX);
+	hdd_event_eapol_log(nbuf, QDF_RX);
 	qdf_dp_trace_log_pkt(adapter->session_id,
-			     skb, QDF_RX, QDF_TRACE_DEFAULT_PDEV_ID);
-	DPTRACE(qdf_dp_trace(skb,
+			     nbuf, QDF_RX, QDF_TRACE_DEFAULT_PDEV_ID);
+	DPTRACE(qdf_dp_trace(nbuf,
 			     QDF_DP_TRACE_RX_HDD_PACKET_PTR_RECORD,
 			     QDF_TRACE_DEFAULT_PDEV_ID,
-			     qdf_nbuf_data_addr(skb),
-			     sizeof(qdf_nbuf_data(skb)), QDF_RX));
-	DPTRACE(qdf_dp_trace_data_pkt(skb, QDF_TRACE_DEFAULT_PDEV_ID,
+			     qdf_nbuf_data_addr(nbuf),
+			     sizeof(qdf_nbuf_data(nbuf)), QDF_RX));
+	DPTRACE(qdf_dp_trace_data_pkt(nbuf, QDF_TRACE_DEFAULT_PDEV_ID,
 				      QDF_DP_TRACE_RX_PACKET_RECORD, 0,
 				      QDF_RX));
 
-	result = hdd_ipa_aggregated_rx_ind(skb);
+	result = hdd_ipa_aggregated_rx_ind(nbuf);
 	if (result == NET_RX_SUCCESS)
 		++adapter->hdd_stats.tx_rx_stats.rx_delivered[cpu_index];
 	else