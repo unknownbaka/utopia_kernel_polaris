bittencourtjulival/eclipse_kernel_xiaomi_stone

qcacld-3.0: Optimize logs in STA disconnect path
Optimize logs in STA disconnect path

Change-Id: I3534f3ab89ebbdd919c02120328013e42d2894f4
CRs-Fixed: 2623124

core/mac/src/pe/lim/lim_process_deauth_frame.c
@@ -149,12 +149,11 @@ lim_process_deauth_frame(struct mac_context *mac, uint8_t *pRxPacketInfo,
 	/* Get reasonCode from Deauthentication frame body */
 	reasonCode = sir_read_u16(pBody);
 
-	pe_nofl_info("Deauth from %pM for %pM mlm state = %d, sme state = %d systemrole = %d RSSI = %d reason %d",
-		     pHdr->sa, pHdr->da,
-		     pe_session->limMlmState,
+	pe_nofl_info("Deauth RX: vdev %d from %pM for %pM RSSI = %d reason %d mlm state = %d, sme state = %d systemrole = %d ",
+		     pe_session->vdev_id, pHdr->sa, pHdr->da, frame_rssi,
+		     reasonCode, pe_session->limMlmState,
 		     pe_session->limSmeState,
-		     GET_LIM_SYSTEM_ROLE(pe_session), frame_rssi,
-		     reasonCode);
+		     GET_LIM_SYSTEM_ROLE(pe_session));
 
 	lim_diag_event_report(mac, WLAN_PE_DIAG_DEAUTH_FRAME_EVENT,
 		pe_session, 0, reasonCode);
@@ -315,7 +314,7 @@ lim_process_deauth_frame(struct mac_context *mac, uint8_t *pRxPacketInfo,
 		cds_flush_logs(WLAN_LOG_TYPE_FATAL,
 			       WLAN_LOG_INDICATOR_HOST_DRIVER,
 			       WLAN_LOG_REASON_DISCONNECT,
-			       true, false);
+			       false, false);
 	}
 
 } /*** end lim_process_deauth_frame() ***/

core/mac/src/pe/lim/lim_process_disassoc_frame.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2019 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2011-2020 The Linux Foundation. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -146,13 +146,11 @@ lim_process_disassoc_frame(struct mac_context *mac, uint8_t *pRxPacketInfo,
 	/* Get reasonCode from Disassociation frame body */
 	reasonCode = sir_read_u16(pBody);
 
-	pe_debug("Received Disassoc frame for Addr: " QDF_MAC_ADDR_STR
-		 "(mlm state=%s, sme state=%d RSSI=%d),"
-		 "with reason code %d [%s] from " QDF_MAC_ADDR_STR,
-		 QDF_MAC_ADDR_ARRAY(pHdr->da),
-		 lim_mlm_state_str(pe_session->limMlmState),
-		 pe_session->limSmeState, frame_rssi, reasonCode,
-		 lim_dot11_reason_str(reasonCode), QDF_MAC_ADDR_ARRAY(pHdr->sa));
+	pe_nofl_info("Disassoc RX: vdev %d from %pM for %pM RSSI = %d reason %d mlm state = %d, sme state = %d systemrole = %d ",
+		     pe_session->vdev_id, pHdr->sa, pHdr->da, frame_rssi,
+		     reasonCode, pe_session->limMlmState,
+		     pe_session->limSmeState,
+		     GET_LIM_SYSTEM_ROLE(pe_session));
 	lim_diag_event_report(mac, WLAN_PE_DIAG_DISASSOC_FRAME_EVENT,
 		pe_session, 0, reasonCode);
 
@@ -313,7 +311,7 @@ lim_process_disassoc_frame(struct mac_context *mac, uint8_t *pRxPacketInfo,
 		cds_flush_logs(WLAN_LOG_TYPE_FATAL,
 			       WLAN_LOG_INDICATOR_HOST_DRIVER,
 			       WLAN_LOG_REASON_DISCONNECT,
-			       true, false);
+			       false, false);
 	}
 } /*** end lim_process_disassoc_frame() ***/
 

unknownbaka/utopia_kernel_polaris

core/mac/src/pe/lim/lim_process_deauth_frame.c
@@ -146,15 +146,11 @@ lim_process_deauth_frame(tpAniSirGlobal pMac, uint8_t *pRxPacketInfo,
 	/* Get reasonCode from Deauthentication frame body */
 	reasonCode = sir_read_u16(pBody);
 
-	pe_nofl_info("Received Deauth frame for Addr: " MAC_ADDRESS_STR
-			"(mlm state = %s, sme state = %d systemrole = %d "
-			"RSSI = %d) with reason code %d [%s] from "
-			MAC_ADDRESS_STR, MAC_ADDR_ARRAY(pHdr->da),
-			lim_mlm_state_str(psessionEntry->limMlmState),
-			psessionEntry->limSmeState,
-			GET_LIM_SYSTEM_ROLE(psessionEntry), frame_rssi,
-			reasonCode, lim_dot11_reason_str(reasonCode),
-			MAC_ADDR_ARRAY(pHdr->sa));
+	pe_nofl_info("Deauth RX: vdev %d from %pM for %pM RSSI = %d reason %d mlm state = %d, sme state = %d systemrole = %d ",
+		     psessionEntry->smeSessionId, pHdr->sa, pHdr->da, frame_rssi,
+		     reasonCode, psessionEntry->limMlmState,
+		     psessionEntry->limSmeState,
+		     GET_LIM_SYSTEM_ROLE(psessionEntry));
 
 	lim_diag_event_report(pMac, WLAN_PE_DIAG_DEAUTH_FRAME_EVENT,
 		psessionEntry, 0, reasonCode);

core/mac/src/pe/lim/lim_process_disassoc_frame.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2018 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2011-2018, 2020 The Linux Foundation. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -145,13 +145,11 @@ lim_process_disassoc_frame(tpAniSirGlobal pMac, uint8_t *pRxPacketInfo,
 	/* Get reasonCode from Disassociation frame body */
 	reasonCode = sir_read_u16(pBody);
 
-	pe_debug("Received Disassoc frame for Addr: " MAC_ADDRESS_STR
-		 "(mlm state=%s, sme state=%d RSSI=%d),"
-		 "with reason code %d [%s] from " MAC_ADDRESS_STR,
-		 MAC_ADDR_ARRAY(pHdr->da),
-		 lim_mlm_state_str(psessionEntry->limMlmState),
-		 psessionEntry->limSmeState, frame_rssi, reasonCode,
-		 lim_dot11_reason_str(reasonCode), MAC_ADDR_ARRAY(pHdr->sa));
+	pe_nofl_info("Disassoc RX: vdev %d from %pM for %pM RSSI = %d reason %d mlm state = %d, sme state = %d systemrole = %d ",
+		     psessionEntry->smeSessionId, pHdr->sa, pHdr->da, frame_rssi,
+		     reasonCode, psessionEntry->limMlmState,
+		     psessionEntry->limSmeState,
+		     GET_LIM_SYSTEM_ROLE(psessionEntry));
 	lim_diag_event_report(pMac, WLAN_PE_DIAG_DISASSOC_FRAME_EVENT,
 		psessionEntry, 0, reasonCode);
 